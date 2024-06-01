5b03ee63a6a61881fc6c5be885ec34b0911508fe
LiamWrigley <Liamwrigley01@gmail.com>
2024-06-02 00:34:46 +1000

    updates

--- scripts/resetStats.js
+++ scripts/resetStats.js
@@ -7,13 +7,13 @@ const resetStats = async () => {
         await mongoose.connect(process.env.MONGO_URI);
 
         // Reset player stats to an empty array
-        const resetStatsResult = await mongoose.connection.db.collection("players").updateMany({}, { $set: { stats: [] } });
-        console.log(chalk.green(`${resetStatsResult.modifiedCount} Player stats reset.`));
+        // const resetStatsResult = await mongoose.connection.db.collection("players").updateMany({}, { $set: { stats: [] } });
+        // console.log(chalk.green(`${resetStatsResult.modifiedCount} Player stats reset.`));
 
         const updateWebhooksResult = await mongoose.connection.db.collection("players").updateMany(
             {},
             { $unset: { matchWebhook: "", history: "" } },
-            { $set: { matchWebhooks: [{ webhookUrl: "https://discord.com/api/webhooks/1244608490568286280/BL2MC7fR9FcusrxQlH4wAWTU40BUJ8se01s08VKR6PGr0HS3Ki6nRV9XVGZPQ3ptuvZu", type: 1, messageId: null, ts: Math.floor(Date.now() / 1000) }] } }
+            { $set: { matchWebhooks: [{ webhookUrl: "https://discord.com/api/webhooks/1246454123289645236/L_M3cuEJwaEJOKTkSB_MSH6cMeadMCdLxAQ4ZYijZqTNOCN3ulUKpKKFMc5x2VY0OeDA", type: 1, messageId: null, ts: Math.floor(Date.now() / 1000) }] } }
         );
         console.log(chalk.green(`${updateWebhooksResult.modifiedCount} 'matchWebhooks' reset and updated for players.`));
 
